ae475dcdcff149ebf1ce14eeaa48dd59bfccc12a
k.malikova <kamilamalikova173@gmail.com>
2024-02-04 22:39:54 +0500

    Pair sum refactored

--- src/main/java/com/leetcode/pointers/PairsSumLessThanTarget.java
+++ src/main/java/com/leetcode/pointers/PairsSumLessThanTarget.java
@@ -4,13 +4,10 @@ import java.util.List;
 
 public class PairsSumLessThanTarget {
     public int countPairs(List<Integer> nums, int target) {
-        nums.sort(Integer::compareTo);
         int k = 0;
-        int sum = 0;
         for (int i = 0; i < nums.size(); i++) {
             for (int j = i + 1; j < nums.size(); j++) {
-                sum = nums.get(i) + nums.get(j);
-                if (sum < target) {
+                if (nums.get(i) + nums.get(j) < target) {
                     k++;
                 }
             }
