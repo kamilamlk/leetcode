039cff1b116ce392e4f5567ed55bb7775e640b15
k.malikova <kamilamalikova173@gmail.com>
2024-02-26 11:06:51 +0500

    Four sum test - not working

--- src/test/java/com/leetcode/pointers/FourSumTest.java
+++ src/test/java/com/leetcode/pointers/FourSumTest.java
@@ -1,9 +1,13 @@
 package com.leetcode.pointers;
 
+import org.junit.jupiter.api.Test;
+
 public class FourSumTest {
     FourSum fourSum = new FourSum();
 
+    @Test
     void test() {
-
+        int[] array = new int[] {1,0,-1,0,-2,2};
+        System.out.println(fourSum.fourSum(array, 0));
     }
 }
